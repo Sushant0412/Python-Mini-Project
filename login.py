082bf480380ea436463354e83152c959c25c8d8a
Sushant0412 <mikesushant05@gmail.com>
2024-03-19 20:13:51 +0530

    login ui updated

--- login.py
+++ login.py
@@ -33,8 +33,9 @@ def login():
 root = tk.Tk()
 root.title("Login Page")
 
-# Load the image
-image = Image.open("./images/loginBg.webp")  # Change the file name accordingly
+# Load the image from desktop
+image_path = "C:/Users/susha/Desktop/Login.png"  # Update the path with your desktop username
+image = Image.open(image_path)
 image = image.resize((800, 600), Image.LANCZOS)  # Resize image to fit window
 
 # Convert Image object to Tkinter PhotoImage object
@@ -55,29 +56,21 @@ root.geometry(f"800x600+{x}+{y}")
 image_label = tk.Label(root, image=photo)
 image_label.place(x=0, y=0)
 
-# Create a frame for the right side (for login box)
-right_frame = tk.Frame(root, bg="#d8dbd8", bd=2, relief="solid")  # Add border to frame
-right_frame.place(relx=0.8, rely=0.5, anchor="center", relwidth=0.3, relheight=0.5)
+# Create and place widgets directly on the main window
+username_label = tk.Label(root, text="Username:", fg="black", font=("Helvetica", 12))
+username_label.place(relx=0.597, rely=0.25, anchor="w")
 
-# Create and place widgets inside the right frame
-username_label = tk.Label(right_frame, text="Username:", bg="#d8dbd8", fg="black", font=("Helvetica", 12))
-username_label.place(relx=0.05, rely=0.2, anchor="w")
+username_entry = tk.Entry(root, width=16, font=("Poppins", 16))
+username_entry.place(relx=0.6, rely=0.32, anchor="w")
 
-username_frame = tk.Frame(right_frame, bg="#d8dbd8")  # Frame for padding
-username_frame.place(relx=0.4, rely=0.2, anchor="w")
-username_entry = tk.Entry(username_frame, width=13, font=("Helvetica", 12))
-username_entry.pack(padx=5, pady=5)  # Add padding
+password_label = tk.Label(root, text="Password:", bg="#d8dbd8", fg="black", font=("Helvetica", 12))
+password_label.place(relx=0.6, rely=0.41, anchor="w")
 
-password_label = tk.Label(right_frame, text="Password:", bg="#d8dbd8", fg="black", font=("Helvetica", 12))
-password_label.place(relx=0.05, rely=0.4, anchor="w")
+password_entry = tk.Entry(root, show="*", width=16, font=("Poppins", 16))
+password_entry.place(relx=0.6, rely=0.48, anchor="w")
 
-password_frame = tk.Frame(right_frame, bg="#d8dbd8")  # Frame for padding
-password_frame.place(relx=0.4, rely=0.4, anchor="w")
-password_entry = tk.Entry(password_frame, show="*", width=13, font=("Helvetica", 12))
-password_entry.pack(padx=5, pady=5)  # Add padding
-
-login_button = tk.Button(right_frame, text="Login", command=login, bg="green", fg="white", width=20, font=("Helvetica", 12))
-login_button.place(relx=0.5, rely=0.7, anchor="center")
+login_button = tk.Button(root, text="Login", command=login, bg="blue", fg="white", width=24, height=2, font=("Helvetica", 12))
+login_button.place(relx=0.74, rely=0.65, anchor="center")
 
 # Run the Tkinter event loop
 root.mainloop()
